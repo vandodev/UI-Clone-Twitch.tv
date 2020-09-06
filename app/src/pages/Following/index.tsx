0e36bfc8a8706842a844f1124bd41301c396f456
Evandro oliveira <vandoaparecido@hotmail.com>
2020-09-06 01:31:09 -0300

    Trabalhando com useMemo

--- app/src/pages/Following/index.tsx
+++ app/src/pages/Following/index.tsx
@@ -1,16 +1,84 @@
 import React from 'react';
 
-import { Text } from 'react-native';
+import { View } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 import Header from '../../components/Header';
 
 import {Wrapper, Container, Main } from './styles';
 
+interface Item {
+  key:string;
+  render: () => JSX.Element;
+  isTitle?: Boolean 
+}
+
 const Following: React.FC = () => {
+
+  const {data, indexes} = React.useMemo(() => {
+    const items : Item[] = [
+
+      {
+        key:'PAGE_HEADING',
+        render: () => <View />
+      },
+
+      {
+        key:'FOLLWED_CATEGORIES',
+        render: () => <View />,
+        isTitle:true
+      },
+      {key:'C1', render: () => <View />},
+
+      {
+        key:'LIVE_CHENNELS',
+        render: () => <View />,
+        isTitle:true
+      },
+      {key:'C2', render: () => <View />},
+
+      {
+        key:'CONTINE_WATCHING',
+        render: () => <View />,
+        isTitle:true
+      },
+      {key:'C3', render: () => <View />},
+
+      {
+        key:'OFLINE_CHENNELS',
+        render: () => <View />,
+        isTitle:true
+      },
+      {key:'C4', render: () => <View />}
+
+     ];
+
+     const indexes : number[] = [];
+
+     items.forEach((item, index) => item.isTitle && indexes.push(index));
+
+     return{
+       data:items,
+       indexes,
+     }
+
+  }, []);
+
   return (
     <Wrapper>
       <Container>
         <Header />
-        <Main />
+        <Main>
+          <FlatList<Item>
+            data={data}
+            renderItem={({item}) => item.render()}
+            keyExtractor={item => item.key}
+            stickyHeaderIndices={indexes}
+
+            //Refresh effect
+            onRefresh={() => {}}
+            refreshing={false}
+          />
+        </Main>
       </Container>
     </Wrapper>
   );
